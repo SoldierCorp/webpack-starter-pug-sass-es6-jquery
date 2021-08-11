5753f16189515cec834d2374bc77b3e67c073428
Edgardo Ramírez <soldiercrp@gmail.com>
2021-08-11 02:07:39 -0500

    REMOVE: Semicolons

--- build/utils.js
+++ build/utils.js
@@ -9,27 +9,27 @@ exports.pages = function (env, folder = '') {
 
   fs.readdirSync(viewsFolder).forEach(view => {
     if (view.split('.')[1] === undefined)
-      return false;
+      return false
 
-    const viewName = view.split('.')[0];
-    const fileName = folder === '' ? `${viewName}/index.html` : `${folder}/${viewName}/index.html`;
+    const viewName = view.split('.')[0]
+    const fileName = folder === '' ? `${viewName}/index.html` : `${folder}/${viewName}/index.html`
     const options = {
       minify: !env === 'development',
       filename: fileName,
       template: `views/${rootPagesFolderName}/${folder}/${view}`,
       inject: true
-    };
+    }
 
     if (env === 'development') {
       options.minify = {
         removeComments: true,
         collapseWhitespace: true,
         removeAttributeQuotes: true
-      };
+      }
     }
 
-    pages.push(new HtmlWebpackPlugin(options));
+    pages.push(new HtmlWebpackPlugin(options))
   })
 
-  return pages;
+  return pages
 }
--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,1 +1,1 @@
-console.log('Demo!');
+console.log('Demo!')
