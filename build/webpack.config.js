209ab5625692cab1d11410e07c80f1bdf296f6d2
Edgardo Ramírez <soldiercrp@gmail.com>
2026-08-18 13:03:57 -0600

    feat: add ads.txt and configure webpack to include it in the build

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -146,6 +146,7 @@ module.exports = (env) => {
     plugins: [
       new CopyWebpackPlugin({
         patterns: [
+          { from: '../ads.txt', to: 'ads.txt' },
           { from: '../manifest.json', to: 'manifest.json' },
           { from: '../browserconfig.xml', to: 'browserconfig.xml' },
           { from: 'assets/images/favicons/android-chrome-192x192.png', to: 'assets/images/android-chrome-192x192.png' },
